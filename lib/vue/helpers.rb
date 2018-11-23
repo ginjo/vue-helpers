d719d5c7ab299797e90de94525584d8e01907c76
wbr <wbr@mac.com>
2018-11-23 01:35:40 -0800

    Debug & refactor to handle capture & concat better.

--- lib/vue/helpers.rb
+++ lib/vue/helpers.rb
@@ -27,7 +27,6 @@ module Vue
     # Not yet, that's a deeper level of customization. Maybe next version. Remember that the helper instance only lasts for one request cycle.
     attr_accessor *%w(
       cache_store
-      template_proc
       component_wrapper
       yield_wrapper
       script_wrapper
@@ -51,21 +50,21 @@ module Vue
   self.script_wrapper = '<script src="#{callback_prefix}/#{key}"></script>'
   
   # TODO: I think this should be a regular method in Helpers module.
-  self.template_proc = Proc.new do |str_or_sym, locals:{}, template_engine:current_template_engine|
-    puts "CAlling template_proc with str_or_sym: \"#{str_or_sym.to_s[0..24].gsub(/\n/, ' ')}\", locals: #{locals}, template_engine: #{template_engine}"
-    tilt_template = \
-      begin
-        case str_or_sym
-        when Symbol; Tilt.new(File.join(Dir.getwd, Vue.views_path, "#{str_or_sym.to_s}.vue.#{template_engine}"))
-        when String; Tilt.template_for(template_engine).new(){str_or_sym}
-        end
-      rescue
-        puts "Template proc: error retrieving template for #{str_or_sym}: #{$!}"
-        nil
-      end
-    #puts "Template proc: context-binding self: #{context.eval('self').inspect}"
-    tilt_template.render(self, **locals) if tilt_template.is_a?(Tilt::Template)
-  end
+  #   self.template_proc = Proc.new do |str_or_sym, locals:{}, template_engine:current_template_engine|
+  #     puts "CAlling template_proc with str_or_sym: \"#{str_or_sym.to_s[0..24].gsub(/\n/, ' ')}\", locals: #{locals}, template_engine: #{template_engine}"
+  #     tilt_template = \
+  #       begin
+  #         case str_or_sym
+  #         when Symbol; Tilt.new(template_path(str_or_sym, template_engine:template_engine))
+  #         when String; Tilt.template_for(template_engine).new(){str_or_sym}
+  #         end
+  #       rescue
+  #         puts "Template proc: error retrieving template for #{str_or_sym}: #{$!}"
+  #         nil
+  #       end
+  #     #puts "Template proc: context-binding self: #{context.eval('self').inspect}"
+  #     tilt_template.render(self, **locals) if tilt_template.is_a?(Tilt::Template)
+  #   end
   
   self.component_wrapper = '
     <#{el_name} #{attributes_string}>
@@ -103,10 +102,12 @@ module Vue
     # Inserts Vue component-call block in html template.
     # Name & file_name refer to file-name.vue.<template_engine> SFC file. Example: products.vue.erb.
     def vue_component(name, root_name:Vue.root_name, attributes:{}, tag:nil, file_name:name, locals:{}, template_engine:current_template_engine, &block)
-      block_content = rendered_block(locals:locals, template_engine:template_engine, &block) if block_given?
+      puts "VUE_COMPONENT called with name: #{name}, root_name: #{root_name}, tag: #{tag}, file_name: #{file_name}, template_engine: #{template_engine}, block_given? #{block_given?}"
+
       component_content = rendered_template(file_name:file_name, locals:locals, template_engine:template_engine)
+      block_content = rendered_block(locals:locals, template_engine:template_engine, &block) if block_given?
       
-      compiled_component_js = compile_component_js(**component_content)
+      compiled_component_js = compile_component_js(name, *component_content)
       vue_roots(root_name).components[name] = compiled_component_js
       
       component_output = compile_component_html_block(
@@ -118,11 +119,11 @@ module Vue
       )
       
       if block_given?
-        puts "VUE_COMPONENT concating content: #{component_output[0..32].gsub(/\n/, ' ')}"
+        puts "Vue_component concating content: #{component_output[0..32].gsub(/\n/, ' ')}"
         concat_content(component_output)
         return nil
       else
-        puts "VUE_COMPONENT returning content: #{component_output[0..32].gsub(/\n/, ' ')}"
+        puts "Vue_component returning content: #{component_output[0..32].gsub(/\n/, ' ')}"
         return component_output
       end
     end
@@ -151,7 +152,13 @@ module Vue
     
     # TODO: Patch this in with the Padrino code.
     def current_template_engine
-      current_engine ||   Vue.template_engine
+      current_engine || Vue.template_engine
+    end
+    
+    def template_path(name, template_engine:current_template_engine)
+      tp = File.join(Dir.getwd, Vue.views_path, "#{name.to_s}.vue.#{template_engine}")
+      puts "Template_path generated: #{tp}"
+      tp
     end
     
     # Stores all root apps defined by vue-helpers, plus their compiled components.
@@ -166,7 +173,11 @@ module Vue
     end
     
     def rendered_template(file_name:nil, locals:{}, template_engine:current_template_engine)
-      parsed_and_rendered_vue_sfc = parse_vue_sfc(file_name.to_sym, locals:locals, template_engine:template_engine)
+      #parsed_and_rendered_vue_sfc = parse_vue_sfc(file_name.to_sym, locals:locals, template_engine:template_engine)
+      template, script = parse_vue_sfc(file_name.to_sym)
+      r_template = render_ruby_template(template, locals:locals, template_engine:template_engine)
+      r_script   = render_ruby_template(script, locals:locals, template_engine:template_engine)
+      [r_template, r_script]
     end
     
     def compile_component_html_block(name:nil, tag:nil, attributes:{}, block_content:'', locals:{})
@@ -191,23 +202,48 @@ module Vue
       ).to_s
     end
 
-    def parse_vue_sfc(template_file, locals:{}, template_engine:current_template_engine)
-      raw = render_ruby_template(template_file, locals:locals, template_engine:template_engine)
-      name = template_file.to_s.split(/[. ]/)[0]
-      a,template,c,script = raw.to_s.match(/(<template>(.*)<\/template>)*.*(<script>(.*)<\/script>)/m).to_a[1..-1]
-      #puts "PARSE_vue_sfc result: #{[template,script].to_yaml}"
-      #[name, template, script]
-      {name:name, vue_template:template, vue_script:script}
+#     def parse_vue_sfc(template_file, locals:{}, template_engine:current_template_engine)
+#       raw = render_ruby_template(template_file, locals:locals, template_engine:template_engine)
+#       name = template_file.to_s.split(/[. ]/)[0]
+#       a,template,c,script = raw.to_s.match(/(<template>(.*)<\/template>)*.*(<script>(.*)<\/script>)/m).to_a[1..-1]
+#       #puts "PARSE_vue_sfc result: #{[template,script].to_yaml}"
+#       #[name, template, script]
+#       {name:name, vue_template:template, vue_script:script}
+#     end
+    
+    def parse_vue_sfc(template_text_or_file)
+      raw_template = begin
+        case template_text_or_file
+        when Symbol; File.read(template_path(template_text_or_file))
+        when String; template_text_or_file
+        end
+      rescue
+        puts "Parse_vue_sfc error getting template file: #{template_text_or_file.to_s[0..32].gsub(/\n/, ' ')}...: #{$!}"
+        nil
+      end
+      a,template,c,script = raw_template.to_s.match(/(<template>(.*)<\/template>)*.*(<script>(.*)<\/script>)/m).to_a[1..-1]
+      #{vue_template:template, vue_script:script}
+      [template, script]
     end
     
     # TODO: Do we need this: 'ERB::Util.html_escape string'. It will convert all html tags like this: "Hi I&#39;m some text. 2 &lt; 3".
     def render_ruby_template(template_text_or_file, locals:{}, template_engine:current_template_engine)
-      puts "RENDER_ruby_template(\"#{template_text_or_file.to_s[0..24].gsub(/\n/, ' ')}\", locals:#{locals}, template_engine:#{template_engine})"
-      instance_exec(template_text_or_file, locals:locals, template_engine:template_engine, &Vue.template_proc)
+      #puts "RENDER_ruby_template(\"#{template_text_or_file.to_s[0..32].gsub(/\n/, ' ')}\", locals:<locals>, template_engine:#{template_engine})"
+        
+      tilt_template = begin
+        case template_text_or_file
+        when Symbol; Tilt.new(template_path(template_text_or_file, template_engine:template_engine))
+        when String; Tilt.template_for(template_engine).new(){template_text_or_file}
+        end
+      rescue
+        puts "Render_ruby_template error building tilt template for #{template_text_or_file.to_s[0..32].gsub(/\n/, ' ')}...: #{$!}"
+        nil
+      end
+      tilt_template.render(self, **locals) if tilt_template.is_a?(Tilt::Template)
     end
 
     #def compile_component_js(name, template, script)
-    def compile_component_js(name:nil, vue_template:nil, vue_script:nil)
+    def compile_component_js(name, vue_template=nil, vue_script=nil)
       if vue_script
         # Yes, this looks weird, but remember we're just replacing the beginning of the script block.
         vue_script.gsub!(/export\s+default\s*\{/, "Vue.component('#{name}', {template: `#{vue_template}`,") << ")"
@@ -233,7 +269,7 @@ module Vue
       
       # {block_content:block_content, vue_sfc:{name:name, vue_template:template, vue_script:script}}
       rendered_template_script = \
-        rendered_template(file_name:file_name.to_sym, locals:locals, template_engine:template_engine).to_h[:script] ||
+        rendered_template(file_name:file_name.to_sym, locals:locals, template_engine:template_engine).to_a[1] ||
         Vue.root_wrapper.interpolate(**locals)
       
       vue_output << rendered_template_script
@@ -278,146 +314,3 @@ module Vue
   end # Source
 
 end
-
-
-
-    # Inserts Vue component-call block in html template.
-    # Name & file_name refer to file-name.vue.<template_engine> SFC file. Example: products.vue.erb.
-    # TODO: Implement template_engine here.
-    # TODO: Implement custom wrapper from template file (like I did with the vue root).
-    #   def vue_component_old(name, root_name = Vue.root_name, attributes:{}, tag:nil, file_name:nil, locals:{}, template_engine:nil)
-    #     
-    #     # Parses SFC file, evaluating template code if exists.
-    #     parsed_sfc = parse_vue_sfc((file_name || name).to_sym, locals:locals)
-    #     
-    #     # Compiles Vue.componenent definition from parsed SFC into a RootApp,
-    #     # and store the resulting JS code in @vue_roots hash.
-    #     vue_roots(root_name).components[name] = compile_component_js(*parsed_sfc)
-    # 
-    #     # Adds 'is' attribute to html vue-component element,
-    #     # if the user specifies an alternate 'tag' (default tag is name-of-component).
-    #     el_name = tag || name
-    #     if tag
-    #       attributes['is'] = name
-    #     end
-    #           
-    #     # Compiles attributes string from given ruby hash.
-    #     attributes_string = attributes.inject(''){|o, kv| o.to_s << "#{kv[0]}=\"#{kv[1]}\" "}
-    #     
-    #     # Captures block of text passed to vue_component method.
-    #     prc = Proc.new if block_given?
-    #     block_content = capture_html(&prc)
-    #     
-    #     puts "CAPTURED block_content: #{block_content}"
-    #     concat_content("CONCAT TEST")
-    #     puts "PROPER HANDLER: #{find_proper_handler}"
-    #     puts "CURRENT ENGINE: #{current_engine}"
-    #     
-    #     # Gets vue-component html wrapper.
-    #     component_block_template = Vue.component_wrapper || "
-    #       <<%= el_name %> <%= attributes_string %>>
-    #         <%= block_content %>
-    #       </<%= el_name %>>
-    #     "
-    #     # Evaluates entire vue-component html block as template, and adds to output buffer.
-    #     # TODO: The template_engine should be variable, but based on what?
-    #     rendered_template = render_ruby_template(component_block_template,
-    #       template_engine:'ERB',
-    #       locals:{
-    #         name:name,
-    #         tag:tag,
-    #         el_name:el_name,
-    #         block_content:block_content,
-    #         attributes_string:attributes_string
-    #       }.merge(locals:locals)
-    #     ).to_s
-    #     
-    #     #buffer << rendered_template
-    #     concat_content(rendered_template)
-    #   end
-    
-
-    #   # Buffer tricks allow addition of 'capture' method for erb. From https://gist.github.com/seanami/496702
-    #   def buffer
-    #     @_out_buf ||= ''
-    #   end
-    #   def capture(buffer)
-    #     pos = buffer.size
-    #     yield
-    #     buffer.slice!(pos..buffer.size)
-    #   end
-
-    
-    #   # These capture/concat methods were found here:
-    #   # https://www.rubydoc.info/gems/darkhelmet-sinatra_more/Sinatra/MarkupPlugin/OutputHelpers#capture_html-instance_method
-    #   # It looks like each templating engine already has some mechanism to handle these,
-    #   # and these methods just give them a standard interface.
-    #   # TODO: Add slim (and others?) to these methods.
-    #   #
-    #   def capture_html(*args, &block)
-    #     if self.respond_to?(:is_haml?) && is_haml?
-    #       block_is_haml?(block) ? capture_haml(*args, &block) : block.call
-    #     elsif has_erb_buffer?
-    #       result_text = capture_erb(*args, &block)
-    #       result_text.present? ? result_text : (block_given? && block.call(*args))
-    #     else # theres no template to capture, invoke the block directly
-    #       puts "CAPTURE_HTML - generic calling block"
-    #       block.call(*args)
-    #     end
-    #   end
-    #   #
-    #   #
-    #   def concat_content(text="")
-    #     if self.respond_to?(:is_haml?) && is_haml?
-    #       haml_concat(text)
-    #     elsif has_erb_buffer?
-    #       erb_concat(text)
-    #     else # theres no template to concat, return the text directly
-    #       puts "CONCAT_CONTENT - generic returning text"
-    #       text
-    #     end
-    #   end
-    #   #
-    #   # Returns true if an erb buffer is detected
-    #   # has_erb_buffer? => true
-    #   def has_erb_buffer?
-    #     !@_out_buf.nil?
-    #   end
-
-
-    #   def build_vue(root_name = Vue.root_name, file_name:root_name, app_name:root_name.camelize, template_engine:Vue.template_engine) 
-    #     root ||= ""
-    #     components = vue_roots(root_name).components
-    #     if components.is_a?(Hash) && components.size > 0 && values=components.values
-    #       root << values.join(";\n")
-    #       root << ";\n"
-    #       
-    #       locals = {
-    #         root_name:     root_name,
-    #         app_name:      app_name,
-    #         file_name:     file_name,
-    #         vue_data_json: vue_roots(root_name).data.to_json
-    #       }
-    #     
-    #       wrapper = (
-    #         # User defined wrapper in class variable.
-    #         Vue.root_wrapper ||
-    #       
-    #         # User defined wrapper in <root_name>.vue.<Vue.template_engine>.
-    #         parse_vue_sfc(file_name.to_sym, template_engine:template_engine, locals: locals)[2] ||
-    #         
-    #         # Default wrapper.
-    #         render_ruby_template("
-    #           var <%= app_name %> = new Vue({
-    #             el: (Vue.root_el || '#<%= root_name %>'),
-    #             data: <%= vue_data_json %>
-    #           })
-    #         ",
-    #           template_engine: 'ERB',
-    #           locals: locals
-    #         )
-    #       ) # end-of-wrapper
-    #       
-    #       root << wrapper
-    #     end
-    #   end 
