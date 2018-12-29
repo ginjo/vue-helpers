ebf7d55311e6db184ed3c4f541c85dacaf6e15c9
wbr <wbr@mac.com>
2018-12-29 02:41:00 -0800

    Added Gemfile.lock to .gitignore. Updated tests to point to correct module & class names.

--- test/vue/helpers_test.rb
+++ test/vue/helpers_test.rb
@@ -2,10 +2,18 @@ require "test_helper"
 
 class Vue::HelpersTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Vue::Helper::VERSION
+    refute_nil ::Vue::Helpers::VERSION
   end
 
   def test_it_does_something_useful
     assert false
+  end
+end
+
+describe Vue::Helpers::Methods do
+  describe 'vue_root' do
+    it 'returns a VueRoot instance' do
+      #assert_kind_of
+    end
   end
 end
--- vue-helpers.gemspec
+++ vue-helpers.gemspec
@@ -43,4 +43,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest", "~> 5.0"
+  spec.add_development_dependency "rack"
+  spec.add_development_dependency "uglifier"
+  spec.add_development_dependency "haml"
+  spec.add_development_dependency "slim"
 end
